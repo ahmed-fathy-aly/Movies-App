94159fc73b5fe964b1f79ff3ed5ca658c11cd88f
ahmed-fathy-aly <ahmed.fathy.aly.93@gmail.com>
2016-06-16 00:38:41 +0200

    master details flow

---
+++ app/src/main/java/com/enterprises/wayne/moviesapp/util/Constants.java
@@ -0,0 +1,8 @@
+package com.enterprises.wayne.moviesapp.util;
+
+/**
+ * Created by ahmed on 6/16/2016.
+ */
+public class Constants
+{
+}
---
+++ app/src/main/java/com/enterprises/wayne/moviesapp/util/Key.java
@@ -0,0 +1,9 @@
+package com.enterprises.wayne.moviesapp.util;
+
+/**
+ * Created by ahmed on 6/16/2016.
+ */
+public class Key
+{
+    public final static String API_KEY = "canYouSeeMe";
+}
